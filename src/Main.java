b40bcdf2e1a4db7164af3afaf218aee1735c4ec3
Djenna07 <dchebil1@stu.vistula.edu.pl>
2026-03-11 17:06:18 +0100

    task1 done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    float weightChebiliEarth=65.0f;
+    float marsGravity=0.38f;
+    float weightChebiliMars;
+    double weightChebiliMarsDouble;
+    int weightChebiliMarsInt;
+    char weightChebiliMarsChar;
+    int charwithOperation;
+
+    //getting weight on Mars
+    weightChebiliMars=weightChebiliEarth*marsGravity;
+    System.out.println("Weight On Mars: "+weightChebiliMars);
+    System.out.println("Hello Vistula");
+    //converting float weight to double
+    weightChebiliMarsDouble=weightChebiliMars;
+    System.out.printf("Weight on Mars with 4 decimal %.4f\n",weightChebiliMarsDouble);
+
+    //converting double weight to int
+    weightChebiliMarsInt= (int)weightChebiliMarsDouble;
+    System.out.println("Mars Weight in Int: "+weightChebiliMarsInt);
+
+    //convert int to char
+    weightChebiliMarsChar =(char) weightChebiliMarsInt;
+    System.out.println("Mars weight in char: "+ weightChebiliMarsChar);
+
+
+    //operation with char
+    charwithOperation= weightChebiliMarsChar +5;
+    System.out.println("Operation with char: "+charwithOperation);
+
 }
